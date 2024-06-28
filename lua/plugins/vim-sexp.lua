aa88145287ebe3524bf4609b60e7711747d2737c
bulte <alexandre.bulte@proton.me>
2024-06-28 21:18:00 +0200

    NEW Plugins to work with s-expressions

---
+++ lua/plugins/vim-sexp.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "guns/vim-sexp",
+    name = "vim-sexp",
+    ft = { "clojure", "fennel" },
+  },
+  {
+    "tpope/vim-sexp-mappings-for-regular-people",
+    name = "vim-sexp-mappings-for-regular-people",
+    ft = { "clojure", "fennel" },
+  },
+  {
+    "tpope/vim-repeat",
+    name = "vim-repeat",
+    ft = { "clojure", "fennel" },
+  },
+}
